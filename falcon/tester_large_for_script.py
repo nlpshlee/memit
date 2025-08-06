948f95e3c855483d5e4750f8014451ee71fe14f2
nlpshlee <tjdgml0820@naver.com>
2025-08-06 17:03:23 +0900

    GPT-J 실험을 위한 코드 완료(Multiple, Sequential)

--- falcon/tester_large_for_script.py
+++ falcon/tester_large_for_script.py
@@ -23,7 +23,25 @@ torch.cuda.manual_seed(SEED)
 torch.backends.cuda.preferred_linalg_library("magma")
 
 
-def get_in_file_path_subject(data_dir, identical_num, batch_idx):
+def get_in_file_path_subject_mul(data_dir, identical_num, num_edits, identical_ratio):
+    in_path = f'{data_dir}/multiple_identical_subjects'
+    file_name = 'mcf_multiple_identical{}_subjects_{}_{}:{}{}.json'
+    file_name = file_name.format(identical_num, num_edits, 10-identical_ratio, identical_ratio, '')
+    in_file_path = f'{in_path}/identical{identical_num}/{file_name}'
+
+    return in_file_path
+
+
+def get_in_file_path_relation_mul(data_dir, identical_num, num_edits, identical_ratio):
+    in_path = f'{data_dir}/multiple_identical_subjects'
+    file_name = 'mcf_multiple_identical{}_subjects_{}_{}:{}{}.json'
+    file_name = file_name.format(identical_num, num_edits, 10-identical_ratio, identical_ratio, '_sr_swap_post')
+    in_file_path = f'{in_path}/identical{identical_num}/{file_name}'
+
+    return in_file_path
+
+
+def get_in_file_path_subject_seq(data_dir, identical_num, batch_idx):
     in_path = f'{data_dir}/sequential_identical_subjects/each'
     file_name = 'mcf_sequential_identical{}_subjects_batch{}{}.json'
     in_file_path = f'{in_path}/identical{identical_num}/' + file_name.format(identical_num, batch_idx, '')
@@ -31,7 +49,7 @@ def get_in_file_path_subject(data_dir, identical_num, batch_idx):
     return in_file_path
 
 
-def get_in_file_path_relation(data_dir, identical_num, batch_idx):
+def get_in_file_path_relation_seq(data_dir, identical_num, batch_idx):
     in_path = f'{data_dir}/sequential_identical_subjects/each'
     file_name = 'mcf_sequential_identical{}_subjects_batch{}{}.json'
     in_file_path = f'{in_path}/identical{identical_num}/' + file_name.format(identical_num, batch_idx, '_sr_swap_post')
@@ -65,10 +83,60 @@ def get_edit_layers(model_name: str):
 
 
 
-def run_memit(alg_name: str, model_name: str, data_dir: str, identical_num: int, num_edits: int, batch_idx: int,
-              layers_subject: list, mode: str, model_dir: str):
+def run_memit_mul(alg_name: str, model_name: str, data_dir: str, identical_num: int, num_edits: int, identical_ratio: int,
+                  layers_subject: list, mode: str, model_dir: str):
 
-    in_file_path = get_in_file_path_subject(data_dir, identical_num, batch_idx)
+    in_file_path = get_in_file_path_subject_mul(data_dir, identical_num, num_edits, identical_ratio)
+    datas_subject = load_datas(in_file_path)
+
+    model_editor = get_model_editor(num_edits, alg_name=alg_name, model_name=model_name, do_init_model=True)
+
+    if data_dir.endswith('org'):
+        model_editor._do_eval_new_model = True
+
+    # 모델 편집
+    model_editor.set_params_external({'layers': layers_subject})
+    model_editor.edit_ext_datas(datas_subject, False, True, True, False, False, False)
+
+    # 모델 저장
+    model_path = get_model_path(model_dir, model_name, identical_num, num_edits, mode)
+    model_editor.model_save(model_path)
+
+
+def run_two_step_mul(alg_name: str, model_name: str, data_dir: str, identical_num: int, num_edits: int, identical_ratio: int,
+                     layers_subject: list, layers_relation: list, mode: str, model_dir: str):
+
+    model_path = get_model_path(model_dir, model_name, identical_num, num_edits, mode)
+
+    if mode.endswith('SUBJECT'):
+        in_file_path = get_in_file_path_subject_mul(data_dir, identical_num, num_edits, identical_ratio)
+
+        model_editor = get_model_editor(num_edits, alg_name=alg_name, model_name=model_name, do_init_model=True)
+        model_editor.set_params_external({'layers': layers_subject})
+        do_edit_test = False
+
+    elif mode.endswith('RELATION'):
+        in_file_path = get_in_file_path_relation_mul(data_dir, identical_num, num_edits, identical_ratio)
+
+        model_editor = get_model_editor(num_edits, alg_name=alg_name, model_name=model_name, do_init_model=False)
+        model_editor.model_load(model_path)
+        model_editor._model.config._name_or_path = model_name
+        model_editor.set_params_external({'layers': layers_relation})
+        do_edit_test = True
+
+        if data_dir.endswith('org'):
+            model_editor._do_eval_new_model = True
+    
+    # 모델 편집 및 저장
+    datas = load_datas(in_file_path)
+    model_editor.edit_ext_datas(datas, False, True, do_edit_test, False, False, False)
+    model_editor.model_save(model_path)
+
+
+def run_memit_seq(alg_name: str, model_name: str, data_dir: str, identical_num: int, num_edits: int, batch_idx: int,
+                  layers_subject: list, mode: str, model_dir: str):
+
+    in_file_path = get_in_file_path_subject_seq(data_dir, identical_num, batch_idx)
     datas_subject = load_datas(in_file_path)
 
     model_path = get_model_path(model_dir, model_name, identical_num, num_edits, mode)
@@ -94,7 +162,7 @@ def run_memit(alg_name: str, model_name: str, data_dir: str, identical_num: int,
     for batch_idx_cur in range(1, batch_idx+1):
         print(f'# falcon.tester_large_for_script.run_memit() batch_idx : {batch_idx}, batch_idx_cur : {batch_idx_cur}\n')
 
-        in_file_path = get_in_file_path_subject(data_dir, identical_num, batch_idx_cur)
+        in_file_path = get_in_file_path_subject_seq(data_dir, identical_num, batch_idx_cur)
         datas_batch = load_datas(in_file_path)
         model_editor.edit_ext_datas(datas_batch, True, False, False, False, False, False)
 
@@ -102,13 +170,13 @@ def run_memit(alg_name: str, model_name: str, data_dir: str, identical_num: int,
 '''
     [SUBJECT, RELATION] 둘 중에 하나만 실행되도록 설계
 '''
-def run_two_step(alg_name: str, model_name: str, data_dir: str, identical_num: int, num_edits: int, batch_idx: int,
-                 layers_subject: list, layers_relation: list, mode: str, model_dir: str):
+def run_two_step_seq(alg_name: str, model_name: str, data_dir: str, identical_num: int, num_edits: int, batch_idx: int,
+                     layers_subject: list, layers_relation: list, mode: str, model_dir: str):
 
     model_path = get_model_path(model_dir, model_name, identical_num, num_edits, mode)
 
     if mode.endswith('SUBJECT'):
-        in_file_path = get_in_file_path_subject(data_dir, identical_num, batch_idx)
+        in_file_path = get_in_file_path_subject_seq(data_dir, identical_num, batch_idx)
         
         if batch_idx == 1:
             model_editor = get_model_editor(num_edits, alg_name=alg_name, model_name=model_name, do_init_model=True)
@@ -120,7 +188,7 @@ def run_two_step(alg_name: str, model_name: str, data_dir: str, identical_num: i
         model_editor.set_params_external({'layers': layers_subject})
 
     elif mode.endswith('RELATION'):
-        in_file_path = get_in_file_path_relation(data_dir, identical_num, batch_idx)
+        in_file_path = get_in_file_path_relation_seq(data_dir, identical_num, batch_idx)
 
         model_editor = get_model_editor(num_edits, alg_name=alg_name, model_name=model_name, do_init_model=False)
         model_editor.model_load(model_path)
@@ -142,31 +210,45 @@ def run_two_step(alg_name: str, model_name: str, data_dir: str, identical_num: i
         for batch_idx_cur in range(1, batch_idx+1):
             print(f'# falcon.tester_large_for_script.run_two_step() batch_idx : {batch_idx}, batch_idx_cur : {batch_idx_cur}\n')
 
-            in_file_path = get_in_file_path_subject(data_dir, identical_num, batch_idx_cur)
+            in_file_path = get_in_file_path_subject_seq(data_dir, identical_num, batch_idx_cur)
             datas_batch = load_datas(in_file_path)
             model_editor.edit_ext_datas(datas_batch, True, False, False, False, False, False)
 
 
-def run(alg_name: str, model_name: str, data_dir, identical_num: int, num_edits: int, batch_idx: int, mode: str, model_dir: str):
+def run(alg_name: str, model_name: str, data_dir, identical_num: int, num_edits: int,
+        identical_ratio: int, batch_idx: int, mode: str, model_dir: str):
+
     print(f'# falcon.tester_large_for_script.run()')
     print(f'\talg_name : {alg_name}')
     print(f'\tmodel_name : {model_name}')
     print(f'\tdata_dir : {data_dir}')
     print(f'\tidentical_num : {identical_num}')
     print(f'\tnum_edits : {num_edits}')
+    print(f'\tidentical_ratio : {identical_ratio}')
     print(f'\tbatch_idx : {batch_idx}')
     print(f'\tmode : {mode}')
     print(f'\tmodel_dir : {model_dir}\n')
 
     layers_subject, layers_relation = get_edit_layers(model_name)
 
-    if mode == 'MEMIT':
-        run_memit(alg_name, model_name, data_dir, identical_num, num_edits, batch_idx,
-                  layers_subject, mode, model_dir)
+    # Multiple Test
+    if identical_ratio != -1 and batch_idx == -1:
+        if mode == 'MEMIT':
+            run_memit_mul(alg_name, model_name, data_dir, identical_num, num_edits, identical_ratio,
+                          layers_subject, mode, model_dir)
+        elif mode.startswith('TWO-STEP'):
+            run_two_step_mul(alg_name, model_name, data_dir, identical_num, num_edits, identical_ratio,
+                             layers_subject, layers_relation, mode, model_dir)
 
-    elif mode.startswith('TWO-STEP'):
-        run_two_step(alg_name, model_name, data_dir, identical_num, num_edits, batch_idx,
-                     layers_subject, layers_relation, mode, model_dir)
+    # Sequential Test
+    elif identical_ratio == -1 and batch_idx != -1:
+        if mode == 'MEMIT':
+            run_memit_seq(alg_name, model_name, data_dir, identical_num, num_edits, batch_idx,
+                    layers_subject, mode, model_dir)
+
+        elif mode.startswith('TWO-STEP'):
+            run_two_step_seq(alg_name, model_name, data_dir, identical_num, num_edits, batch_idx,
+                        layers_subject, layers_relation, mode, model_dir)
 
 
 def run_test_for_script():
@@ -196,7 +278,8 @@ if __name__ == "__main__":
     parser.add_argument('--data_dir', type=str, required=True, help="Data dir")
     parser.add_argument('--identical_num', type=int, required=True, help="Number of identicals")
     parser.add_argument('--num_edits', type=int, required=True, help="Number of edits")
-    parser.add_argument('--batch_idx', type=int, required=True, help="Batch index")
+    parser.add_argument('--identical_ratio', type=int, required=False, default=-1, help="Identical ratio")
+    parser.add_argument('--batch_idx', type=int, required=False, default=-1, help="Batch index")
     parser.add_argument('--mode', type=str, required=True, help="Mode flag")
     parser.add_argument('--model_dir', type=str, required=True, help="Model dir")
     args = parser.parse_args()
@@ -206,6 +289,7 @@ if __name__ == "__main__":
     data_dir = args.data_dir
     identical_num = args.identical_num
     num_edits = args.num_edits
+    identical_ratio = args.identical_ratio
     batch_idx = args.batch_idx
     mode = args.mode
     model_dir = args.model_dir
@@ -213,7 +297,8 @@ if __name__ == "__main__":
     if model_name == 'gpt-j':
         model_name = 'EleutherAI/gpt-j-6B'
 
-    run(alg_name, model_name, data_dir, identical_num, num_edits, batch_idx, mode, model_dir)
+    run(alg_name, model_name, data_dir, identical_num, num_edits,
+        identical_ratio, batch_idx, mode, model_dir)
     
     '''
         매 편집 스텝마다, 모델을 저장하고 불러오는 것 자체는 문제가 없음
---
+++ runner_large_muls.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# 오류가 발생하면 즉시 정지
+set -e
+
+DATA_DIR='./data/preprocessing_new'
+LOG_DIR='./logs'
+MODEL_DIR='./models'
+
+
+ALG_NAME="MEMIT"
+# MODEL_NAME="gpt2-xl"
+MODEL_NAME="gpt-j"
+IDENTICAL_NUMS=(2 3 4 5 6 7 8 9 10)
+NUM_EDITS_LIST=(1000 1000 1000 1000 1000 1000 1000 1000 1000)
+# IDENTICAL_RATIOS=(0 1 2 3 4 5 6 7 8 9 10)
+IDENTICAL_RATIOS=(10)
+MODES=("MEMIT" "TWO-STEP_SUBJECT" "TWO-STEP_RELATION")
+
+
+for IDX in "${!IDENTICAL_NUMS[@]}"; do
+	IDENTICAL_NUM=${IDENTICAL_NUMS[$IDX]}
+	NUM_EDITS=${NUM_EDITS_LIST[$IDX]}
+	DATA_SIZE=$NUM_EDITS
+
+	LOG_FILE_PATH=$LOG_DIR"/log_large_mul_"$ALG_NAME"_"$MODEL_NAME"_identcial"$IDENTICAL_NUM"_"$DATA_SIZE".txt"
+	echo "Removing existing log file : $LOG_FILE_PATH"
+	rm -f "$LOG_FILE_PATH"
+
+	for IDENTICAL_RATIO in "${IDENTICAL_RATIOS[@]}"; do
+		for MODE in "${MODES[@]}"; do
+			echo "alg_name : $ALG_NAME"
+			echo "model_name : $MODEL_NAME"
+			echo "data_dir : $DATA_DIR"
+			echo "identical_num : $IDENTICAL_NUM"
+			echo "num_edits : $NUM_EDITS"
+			echo "identical_ratio : $IDENTICAL_RATIO"
+			echo "mode : $MODE"
+			echo "log_file_path : $LOG_FILE_PATH"
+			echo "model_dir : $MODEL_DIR"
+			echo ""
+
+			python -u -m falcon.tester_large_for_script \
+				--alg_name=$ALG_NAME \
+				--model_name=$MODEL_NAME \
+				--data_dir=$DATA_DIR \
+				--identical_num=$IDENTICAL_NUM \
+				--num_edits=$NUM_EDITS \
+				--identical_ratio=$IDENTICAL_RATIO \
+				--mode=$MODE \
+				--model_dir=$MODEL_DIR >> $LOG_FILE_PATH 2>&1
+		done
+	done
+done
+
+echo "# Terminate all processes!"
+
